0f9445c3307bb3ed2e1315d47b16383918d5fa58
Brisa Nrown <brisan.brown@gmail.com>
2019-10-02 15:06:14 -0400

    Created a Skateboard  class and initialized the member data.

---
+++ Skateboard.cpp
@@ -0,0 +1,7 @@
+#include "Skateboard.h"
+
+
+Skateboard::Skateboard()
+{
+    mileage = rand();
+}
---
+++ Skateboard.h
@@ -0,0 +1,14 @@
+#ifndef DRIVINGSIMULATOR_BICYCLE_H
+#define DRIVINGSIMULATOR_BICYCLE_H
+
+#include "Skateboard.h"
+
+class Skateboard : public Skateboard
+{
+
+private:
+    int mileage;
+
+public:
+
+}
